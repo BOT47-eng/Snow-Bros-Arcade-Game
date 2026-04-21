30629b97acbd5dfbe7ee4f81aba90c9a3f9643c2
DaniyalZaki <daniyalzaki980@gmail.com>
2026-04-21 22:13:49 +0500

    Added manual configuration of keybinds
    Using Keyboard::Scancode added temporary storage and use of custom keybinds for input manager

--- InputManager.cpp
+++ InputManager.cpp
@@ -8,7 +8,22 @@ class InputManager {
 	private:
 		bool player1Shot, player2Shot;
 		bool player1Jumped, player2Jumped;
+		bool gamePaused, debugHit;
+
+		//0-left, 1-right, 2-up, 3-shoot
+		Keyboard::Scancode player1Keys[4] = { Keyboard::Scancode::Left , Keyboard::Scancode::Right , Keyboard::Scancode::Space , Keyboard::Scancode::J };
+		Keyboard::Scancode player2Keys[4] = { Keyboard::Scancode::A , Keyboard::Scancode::D , Keyboard::Scancode::W , Keyboard::Scancode::Space };
+
 	public:
+		void setKeys(Keyboard::Scancode player1[4], Keyboard::Scancode player2[4])
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				player1Keys[i] = player1[i];
+				player2Keys[i] = player2[i];
+			}
+		}
+
 		//Called each cycle of game loop to reset movement
 		void resetInput()
 		{
@@ -16,27 +31,29 @@ class InputManager {
 			player2Shot = false;
 			player1Jumped = false;
 			player2Jumped = false;
+			gamePaused = false;
+			debugHit = false;
 		}
 
 		//Movement does not use events and hence is smooth/continuous
 		bool p1Left()
 		{
-			return Keyboard::isKeyPressed(Keyboard::Scancode::Left);
+			return Keyboard::isKeyPressed(player1Keys[0]);
 		}
 
 		bool p2Left()
 		{
-			return Keyboard::isKeyPressed(Keyboard::Scancode::A);
+			return Keyboard::isKeyPressed(player2Keys[0]);
 		}
 
 		bool p1Right()
 		{
-			return Keyboard::isKeyPressed(Keyboard::Scancode::Right);
+			return Keyboard::isKeyPressed(player1Keys[1]);
 		}
 
 		bool p2Right()
 		{
-			return Keyboard::isKeyPressed(Keyboard::Scancode::Left);
+			return Keyboard::isKeyPressed(player2Keys[1]);
 		}
 
 		//Both jump and shoot uses events so they can only happen once per press
@@ -44,20 +61,21 @@ class InputManager {
 		{
 			if (event.type == Event::KeyPressed)
 			{
-				switch (event.key.scancode)
+				if (event.key.scancode == player1Keys[2])
 				{
-					case Keyboard::Scancode::W:
-						player1Jumped = true;
-						break;
-					case Keyboard::Scancode::Up:
-						player2Jumped = true;
-						break;
-					case Keyboard::Scancode::J:
-						player1Shot = true;
-						break;
-					case Keyboard::Scancode::Space:
-						player2Shot = true;
-						break;
+					player1Jumped = true;
+				}
+				else if (event.key.scancode == player2Keys[2])
+				{
+					player2Jumped = true;
+				}
+				else if (event.key.scancode == player1Keys[3])
+				{
+					player1Shot = true;
+				}
+				else if (event.key.scancode == player2Keys[3])
+				{
+					player2Shot = true;
 				}
 			}
 		}
